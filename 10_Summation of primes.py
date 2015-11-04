32822c79f9e82815cbfa6ec7d7b6e1465b829ac1
n00blet <pritam.rakshi@gmail.com>
2015-11-05 01:34:06 +0530

    sieve

--- 10_Summation of primes.py
+++ 10_Summation of primes.py
@@ -3,5 +3,24 @@
 The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 Find the sum of all the primes below two million.
+
+Follow this algo for faster solution --> https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 """
+from math import sqrt
+import time
+s = time.time()
+
+def sum_primes(limit):
+    sieve = range(limit+1); sieve[1] = 0
+    for n in xrange(2, int(sqrt(limit))+1):
+        if sieve[n] > 0:
+            for i in xrange(n*n, limit+1, n): sieve[i] = 0
+    return sum(sieve)
+
+print time.time() - s
+print sum_primes(2000000)
+
+
+
+
 
